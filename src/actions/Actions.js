f2107f87c6e94ea90ac3a120fbb53cd78c02447e
roshow <roshow@gmail.com>
2016-09-17 01:57:38 -0400

    does not make rankings call if we alredy have that list in our object

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -5,18 +5,26 @@ export const GET_RANKINGS_RESPONSE = 'GET_RANKINGS_RESPONSE';
 export const GET_RANKINGS_ERROR = 'GET_RANKINGS_ERROR';
 
 export function getRankings (division, season) {
+
+	const rankingKey = `${division}${season}`;
+	
 	return ( dispatch, getState ) => {
+
+		if (getState().rankings[rankingKey]) {
+			return;
+		}
 		
 		dispatch({
 			type: GET_RANKINGS_REQUEST
 		});
+
 		setTimeout( () => {
 			fetch(`https://nycxwing-league-api.herokuapp.com/api/rankings/season/${season}/division/${division}`)
 			.then( response => response.json() )
 			.then( rankings => dispatch({
 	  		type: GET_RANKINGS_RESPONSE,
 	  		ranking: {
-	  			[`${division}${season}`]: rankings
+	  			[rankingKey]: rankings
 	  		}
 	    }));
 	  }, 3000);
